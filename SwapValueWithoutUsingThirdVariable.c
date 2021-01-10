678de38b4f7c3138ba7cbefc20436efa30e34ced
THALLAPELLI JAYANTH <75982026+jayanth9676@users.noreply.github.com>
2021-01-10 20:52:51 +0530

    Update SwapValueWithoutUsingThirdVariable.c

--- SwapValueWithoutUsingThirdVariable.c
+++ SwapValueWithoutUsingThirdVariable.c
@@ -7,9 +7,7 @@ int main()
     int a, b;
     printf("Enter two no :\n");
     scanf("%d%d",&a,&b);
-    a = a^b;
-    b = a^b;
-    a=  a^b;
+    b=a+b-(a=b);
     printf("After swapping value of a and b : %d,%d",a,b);
     return 0;
 }
